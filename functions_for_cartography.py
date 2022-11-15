09204949adfbe6067d36a0f960593dbc55177a57
Florent Bickert <florent.bickert@airseas.com>
2022-11-15 09:12:00 +0100

    reformating of the new cartography options

---
+++ functions_for_cartography.py
@@ -0,0 +1,49 @@
+from math import sin, cos, acos, pi
+
+
+def dms2dd(d, m, s):
+    """Convertit un angle "degrés minutes secondes" en "degrés décimaux"
+    """
+    return d + m / 60 + s / 3600
+
+
+def dd2dms(dd):
+    """Convertit un angle "degrés décimaux" en "degrés minutes secondes"
+    """
+    d = int(dd)
+    x = (dd - d) * 60
+    m = int(x)
+    s = (x - m) * 60
+    return d, m, s
+
+
+def deg2rad(dd):
+    """Convertit un angle "degrés décimaux" en "radians"
+    """
+    return dd / 180 * pi
+
+
+def rad2deg(rd):
+    """Convertit un angle "radians" en "degrés décimaux"
+    """
+    return rd / pi * 180
+
+
+def distance_gps(lat_a, lon_a, lat_b, lon_b):
+    """Retourne la distance en mètres entre les 2 points A et B connus grâce à
+       leurs coordonnées GPS (en degrés décimaux).
+    """
+    # cooordonnées GPS en radians du 1er point
+    lat_a = deg2rad(lat_a)
+    lon_a = deg2rad(lon_a)
+
+    # cooordonnées GPS en radians du 2ème point
+    lat_b = deg2rad(lat_b)
+    lon_b = deg2rad(lon_b)
+
+    # Rayon de la terre en mètres (sphère IAG-GRS80)
+    earth_radius = 6378137
+    # angle en radians entre les 2 points
+    s = acos(sin(lat_a) * sin(lat_b) + cos(lat_a) * cos(lat_b) * cos(abs(lon_b - lon_a)))
+    # distance entre les 2 points, comptée sur un arc de grand cercle
+    return s * earth_radius
--- nemo_to_gpx.py
+++ nemo_to_gpx.py
@@ -2,59 +2,8 @@ import requests
 from datetime import datetime
 from gpxpy import gpx
 import json
-import pyproj
+from functions_for_cartography import distance_gps
 
-from math import sin, cos, acos, pi
- 
-#############################################################################
-def dms2dd(d, m, s):
-    """Convertit un angle "degrés minutes secondes" en "degrés décimaux"
-    """
-    return d + m/60 + s/3600
- 
-#############################################################################
-def dd2dms(dd):
-    """Convertit un angle "degrés décimaux" en "degrés minutes secondes"
-    """
-    d = int(dd)
-    x = (dd-d)*60
-    m = int(x)
-    s = (x-m)*60
-    return d, m, s
- 
-#############################################################################
-def deg2rad(dd):
-    """Convertit un angle "degrés décimaux" en "radians"
-    """
-    return dd/180*pi
- 
-#############################################################################
-def rad2deg(rd):
-    """Convertit un angle "radians" en "degrés décimaux"
-    """
-    return rd/pi*180
- 
-#############################################################################
-def distanceGPS(latA, longA, latB, longB):
-    """Retourne la distance en mètres entre les 2 points A et B connus grâce à
-       leurs coordonnées GPS (en degrés décimaux).
-    """
-    # cooordonnées GPS en radians du 1er point
-    latA = deg2rad(latA)
-    longA = deg2rad(longA)
-
-    # cooordonnées GPS en radians du 2ème point
-    latB = deg2rad(latB)
-    longB = deg2rad(longB)
-    
-    # Rayon de la terre en mètres (sphère IAG-GRS80)
-    RT = 6378137
-    # angle en radians entre les 2 points
-    S = acos(sin(latA)*sin(latB) + cos(latA)*cos(latB)*cos(abs(longB-longA)))
-    # distance entre les 2 points, comptée sur un arc de grand cercle
-    return S*RT
- 
-#############################################################################
 
 def datetime_to_api_str(date: datetime):
     """
@@ -114,7 +63,7 @@ def nemo_to_gpx(start_date: datetime, end_date: datetime, delta_time_minutes: in
         '2CqualityOverall%2CmobileCountryCode%2CmobileType%2CradarEchoId%2CmobileImo%2CmobileCallSign&' +
         f'from={start_date_str}&to={end_date_str}&dateType=creation&mode=default')
     data = response.json()
-
+    # Create GPX file
     if data['data']:
         n_points = len(data['data'])
         print(f'Number of points from the API: {n_points}')
@@ -128,15 +77,16 @@ def nemo_to_gpx(start_date: datetime, end_date: datetime, delta_time_minutes: in
         gpx_track.segments.append(gpx_segment)
         # Create points
         this_last_date = datetime.strptime(data['data'][0]['locDate'], "%Y-%m-%d_%H:%M:%S")
+        # Initialize the number of points in the GPX file
         n_points_out = 0
+        # Initialize the average speed
         speed_average = 0
+        # Initialize the number of points used to average the data
         n_points_average = 0
         n_index = 0
         total_dist = 0
-        latA=0
-        longA=0
-        latB=0
-        longB=0
+        lat_a = 0
+        long_a = 0
         for it_response in data['data']:
             this_date = datetime.strptime(it_response['locDate'], "%Y-%m-%d_%H:%M:%S")
             if (this_date - this_last_date).total_seconds() / 60 > delta_time_minutes:
@@ -150,12 +100,12 @@ def nemo_to_gpx(start_date: datetime, end_date: datetime, delta_time_minutes: in
                                                f"COG: {it_response['heading']}"))
                     this_last_date = this_date
                     n_points_out += 1
-                    
+
                     # Add speed to average if different from 0
-                    if (float(it_response['speed']) != 0):
+                    if float(it_response['speed']) != 0:
                         speed_average += float(it_response['speed'])
                         n_points_average += 1
-                    
+
             # Add a waypoint to the last waypoint
             if it_response == data['data'][-1]:
                 print(f'\nLast known position: {this_date}')
@@ -166,28 +116,28 @@ def nemo_to_gpx(start_date: datetime, end_date: datetime, delta_time_minutes: in
                                           name=f"Date: {this_date} UTC, SOG: {it_response['speed']}, "
                                                f"COG: {it_response['heading']}")
                 gpx_data.waypoints.append(last_wp)
-
-            longB=it_response['loc'][0]                    
-            latB=it_response['loc'][1]            
-            #Compute distance between two last points after first point
-            if (n_index>2 and float(it_response['speed']) != 0 and not(longA==longB and latA==latB)):
-                dist = distanceGPS(latA, longA, latB, longB)/1852
-                format_dist = "{:.3f}".format(dist)
+            # Compute total distance and speed
+            long_b = it_response['loc'][0]
+            lat_b = it_response['loc'][1]
+            # Compute distance between two last points after first point
+            if n_index > 2 and float(it_response['speed']) != 0 and not (long_a == long_b and lat_a == lat_b):
+                dist = distance_gps(lat_a, long_a, lat_b, long_b) / 1852
                 total_dist += dist
-                
-            n_index+=1
-            longA=it_response['loc'][0]                    
-            latA=it_response['loc'][1]
+
+            n_index += 1
+            long_a = it_response['loc'][0]
+            lat_a = it_response['loc'][1]
         # Define gpx output file
         with open(file_name, 'w') as f:
             f.write(gpx_data.to_xml())
         print(f'Created GPX file "{file_name}" with {n_points_out} points')
-        
-        #Print average speed in kts (*1,945 => Pourquoi ce coef et non pas 1,852 ? sur fishweb-nemo, la vitesse est en noeuds, alors que la vitesse renvoyée est dans une unité inconnue, donc coef trouvé empiriquement... )
-        speed_average=(speed_average/n_points_average)*1.945
-        format_speed="{:.2f}".format(speed_average)
+
+        # Print average speed in kts (*1,945 => Pourquoi ce coef et non pas 1,852 ? sur fishweb-nemo, la vitesse est
+        # en noeuds, alors que la vitesse renvoyée est dans une unité inconnue, donc coef trouvé empiriquement... )
+        speed_average = (speed_average / n_points_average) * 1.945
+        format_speed = "{:.2f}".format(speed_average)
         print(f'Average speed = {format_speed} kts')
 
-        #Print total distance in nautic miles
+        # Print total distance in nautical miles
         format_total_dist = "{:.1f}".format(total_dist)
         print(f'Total distance = {format_total_dist} nmi')
