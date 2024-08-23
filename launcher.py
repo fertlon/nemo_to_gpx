037cf5d8f207f24d9dfc8f21318600cab7e521a8
fertlon <florent.bickert49@gmail.com>
2024-08-23 16:34:31 +0200

    updated the enb date following the end of ScubaLibre's round-the-world trip

--- launcher.py
+++ launcher.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 # Define the time window
 start_date = datetime(2022, 10, 27, 6, 30, tzinfo=timezone.utc)
 # UTC departure time from Oléron
-end_date = datetime.now(timezone.utc)
+end_date = datetime(2024, 8, 17, 14, 0, tzinfo=timezone.utc)#datetime.now(timezone.utc)
 
 # Define the output gpx file name
 file_name = "heremoana_track.gpx"
